46e5d6a977aac99bbcee7875b0df4d8a8b795d93
bharatrajtj <65009556+bharatrajtj@users.noreply.github.com>
2024-04-20 16:10:25 -0400

    Create variables.tf

---
+++ AWS_VPC/modules/s3/variables.tf
@@ -0,0 +1,11 @@
+variable "AMI" {
+  description = "AMI value for EC2"
+}
+
+variable "Instance" {
+  description = "Instance type for EC2"
+}
+
+variable "subn" {
+  description = "Instance type for EC2"
+}
